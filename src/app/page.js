0036dfc87bb4d93cd0349c0afcfe6f77eac4065c
Sunanda-05 <mannasunanda11705@gmail.com>
2025-09-08 20:32:52 +0530

    Graforce's Solution Add

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/sections/home/Hero";
 import AboutUs from "@/components/sections/home/AboutUs";
 import Brands from "@/components/sections/home/Brands";
 import WorldNeedsUs from "@/components/sections/home/WorldNeedsUs";
-import Solution from "@/components/sections/home/Solution";
+import Solution from "@/components/sections/home/SolutionSection";
 import Video from "@/components/sections/home/Video";
 import FinalCTA from "@/components/shared/finalCta/FinalCTA";
 
