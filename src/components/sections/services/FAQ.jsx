70505861ee622c5db45e8fb45a884280928d8967
Shadow <das840kunal@gmail.com>
2025-09-12 21:58:49 +0530

    fix:BoltMax-W

--- src/components/sections/services/FAQ.jsx
+++ src/components/sections/services/FAQ.jsx
@@ -32,7 +32,8 @@ const faqs = [
 
 export default function FAQSection() {
   return (
-    <SectionWrapper className="max-w-[1460px] mx-auto bg-white">
+    <div className="bg-white">
+    <SectionWrapper className="max-w-[2000px] mx-auto">
       <div className="mb-[10px] text-center">
         <GradientBadge text="FAQs" icon={<TbSquareRotatedFilled />}/>
         <h2 className="text-[32px] md:text-[64px] w-fit mx-auto">
@@ -53,5 +54,6 @@ export default function FAQSection() {
         ))}
       </motion.div>
     </SectionWrapper>
+    </div>
   );
 }
