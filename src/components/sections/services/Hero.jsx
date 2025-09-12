3386dd3ec7988dd116f2ef096531f541635d3217
Shadow <das840kunal@gmail.com>
2025-09-12 20:30:29 +0530

    fix:Positioning

--- src/components/sections/services/Hero.jsx
+++ src/components/sections/services/Hero.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import ScrollReveal from "@/wrappers/ScrollReveal";
-import Path from "@/assets/home/hero/Path.png";
-import Rectangle from "@/assets/home/hero/Rectangle-5.png";
+
 import HeroImg from "@/assets/service/hero/Hero.png";
-import FloatingTip from "@/components/utils/heroutils/FloatingTip";
+
 import FloatingBolt from "@/components/utils/heroutils/FloatingBolt";
 import FloatupButton from "@/components/utils/buttons/FloatupButton";
 import ElevatedButton from "@/components/utils/buttons/ElevatedButton";
-import Customer1 from "@/assets/home/hero/Ellipse-10.png";
-import Customer2 from "@/assets/home/hero/Ellipse-11.png";
-import Customer3 from "@/assets/home/hero/Ellipse-15.png";
-import vector from "@/assets/service/hero/Vector.png";
+
+import vector from "@/assets/service/hero/Vector.webp";
 
 import SectionWrapper from "@/wrappers/SectionWrapper";
 
 const Hero = () => {
   return (
-      <div className="bg-no-repeat lg:bg-repeat bg-cover xl:bg-contain bg-center"
+      <div className="bg-no-repeat lg:bg-repeat bg-cover xl:bg-contain bg-center bg-white"
             style={{ backgroundImage: `url(${vector.src})`, 
                     //backgroundSize: '60%',
                     backgroundPosition: 'center',
@@ -27,11 +24,11 @@ const Hero = () => {
       >
       <div className="max-w-[2000px] mx-auto">
       <div className="h-screen relative overflow-hidden ">
-        <Image src={HeroImg} alt="HeroImg" className="absolute bottom-0" />
+        <Image src={HeroImg} alt="HeroImg" className="absolute bottom-0 " />
 
         <ScrollReveal>
         <div className="h-1/2 absolute top-32 left-0 w-full flex flex-col gap-8 items-center z-10">
-          <h1 className="text-5xl lg:text-7xl 2xl:text-8xl text-center lg:max-2xl:max-w-4xl">Hydrogen. Zero CO2 Emissions.</h1>
+          <h1 className="text-5xl lg:text-7xl 2xl:text-8xl text-center lg:max-2xl:max-w-4xl 2xl:max-w-6xl">Hydrogen. Zero CO2 Emissions.</h1>
           <div className="flex md:flex-wrap md:flex-row gap-4 flex-col items-center justify-center  w-full px-8">
             <FloatupButton className="md:!w-fit" icon={"❯"}>Talk to an Expert</FloatupButton>
             <ElevatedButton className="md:!w-fit">Learn More</ElevatedButton>
@@ -41,8 +38,9 @@ const Hero = () => {
 
       </div>
 
-        <FloatingBolt className="bottom-55 left-1/6 md:top-60 md:left-9/50 xl:top-40" size="w-16 h-16" />
-        <FloatingBolt className="bottom-45 right-1/7 md:top-100 md:right-1/5 lg:top-90 lg:right-1/4" size="w-10 h-10"  />
+        <FloatingBolt className="top-120 left-1/6 md:top-60 md:left-9/50 xl:top-40 2xl:left-4/25" size="w-16 h-16" />
+        <FloatingBolt className="top-140 right-1/7 md:top-100 md:right-1/5 lg:top-90 lg:right-1/4" size="w-10 h-10"  />
+      
       </div>
     </div>
   );
--- src/components/sections/services/Solutions.jsx
+++ src/components/sections/services/Solutions.jsx
@@ -2,7 +2,7 @@ import react from 'react';
 
 const Solutions = () => {
   return (
-    <div>Solutions Section</div>
+    <div className='bg-white'>Solutions Section</div>
   )
 }
 
