3b2547468e3367c4d28e14cc3527f3ee1fb6464d
Shadow <das840kunal@gmail.com>
2025-09-12 15:50:58 +0530

    add:ServicesPage/Hero

--- src/components/sections/services/Hero.jsx
+++ src/components/sections/services/Hero.jsx
@@ -17,14 +17,21 @@ import SectionWrapper from "@/wrappers/SectionWrapper";
 
 const Hero = () => {
   return (
-      <div>
-      <div className="max-w-[1460px] mx-auto">
-      <div className="h-screen relative overflow-hidden">
+      <div className="bg-no-repeat lg:bg-repeat bg-cover xl:bg-contain bg-center"
+            style={{ backgroundImage: `url(${vector.src})`, 
+                    //backgroundSize: '60%',
+                    backgroundPosition: 'center',
+                    opacity: 1,
+                    mixBlendMode: 'color',
+          }}
+      >
+      <div className="max-w-[2000px] mx-auto">
+      <div className="h-screen relative overflow-hidden ">
         <Image src={HeroImg} alt="HeroImg" className="absolute bottom-0" />
 
         <ScrollReveal>
         <div className="h-1/2 absolute top-32 left-0 w-full flex flex-col gap-8 items-center z-10">
-          <h1 className="text-5xl lg:max-xl:text-7xl xl:text-8xl text-center lg:max-xl:max-w-5xl">Hydrogen. Zero CO2 Emissions.</h1>
+          <h1 className="text-5xl lg:text-7xl 2xl:text-8xl text-center lg:max-2xl:max-w-4xl">Hydrogen. Zero CO2 Emissions.</h1>
           <div className="flex md:flex-wrap md:flex-row gap-4 flex-col items-center justify-center  w-full px-8">
             <FloatupButton className="md:!w-fit" icon={"❯"}>Talk to an Expert</FloatupButton>
             <ElevatedButton className="md:!w-fit">Learn More</ElevatedButton>
@@ -34,8 +41,8 @@ const Hero = () => {
 
       </div>
 
-        {/* <FloatingBolt className="top-40 left-9/50" size="w-16 h-16" />
-        <FloatingBolt className="bottom-70 right-1/4" /> */}
+        <FloatingBolt className="bottom-55 left-1/6 md:top-60 md:left-9/50 xl:top-40" size="w-16 h-16" />
+        <FloatingBolt className="bottom-45 right-1/7 md:top-100 md:right-1/5 lg:top-90 lg:right-1/4" size="w-10 h-10"  />
       </div>
     </div>
   );
