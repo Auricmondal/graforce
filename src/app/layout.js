d6c3fa461d64ba2b52752e167942c08a784e10bf
Anix003 <baranish2003@gmail.com>
2025-09-14 01:11:11 +0530

    add: integrate Lenis for smooth scrolling experience

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import LoaderProvider from "@/wrappers/LoaderProvider";
 import { LoaderContextProvider } from "@/contexts/LoaderContext";
 import ContactModal from "@/components/shared/navbar/nav/ContactModal";
 import { ContactModalProvider } from "@/contexts/ContactModalContext";
-import { FaBullseye } from "react-icons/fa";
+import { ReactLenis } from "@/components/utils/lenis/LenisProvider";
 
 export const metadata = {
   title: "Graforce",
@@ -18,23 +18,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <LoaderContextProvider>
-        <ContactModalProvider>
-          <body className={`${oxygen.variable} ${dmSans.variable} antialiased`}>
-            <LoaderProvider>
-              <div className="relative z-10">
-                <Navbar />
-                <ProgressProvider>{children}</ProgressProvider>
-              </div>
-              <div className="relative z-10 pointer-events-none">
-                <FooterRevealer />
-              </div>
-              <Footer />
-              <ContactModal clickOutside={false} />
-            </LoaderProvider>
-          </body>
-        </ContactModalProvider>
-      </LoaderContextProvider>
+      <ReactLenis root>
+        <LoaderContextProvider>
+          <ContactModalProvider>
+            <body
+              className={`${oxygen.variable} ${dmSans.variable} antialiased`}
+            >
+              <LoaderProvider>
+                <div className="relative z-10">
+                  <Navbar />
+                  <ProgressProvider>{children}</ProgressProvider>
+                </div>
+                <div className="relative z-10 pointer-events-none">
+                  <FooterRevealer />
+                </div>
+                <Footer />
+                <ContactModal clickOutside={false} />
+              </LoaderProvider>
+            </body>
+          </ContactModalProvider>
+        </LoaderContextProvider>
+      </ReactLenis>
     </html>
   );
 }
