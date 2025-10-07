1cc6b889bd18e1fffd0fa6be32a1e0281fecffe3
Sunanda-05 <mannasunanda11705@gmail.com>
2025-10-07 20:47:57 +0530

    What do we do section add

--- src/components/sections/home/WhatDoWeDo.jsx
+++ src/components/sections/home/WhatDoWeDo.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Image from "next/image";
 import SectionLabel from "@/components/utils/badges/SectionLabel";
 import globeImg from "@/assets/home/globe.webp";
+import FloatingTip from "@/components/utils/heroUtils/FloatingTip";
+import AnimatedHeader from "@/components/utils/animations/AnimatedHeader";
 
 const WhatDoWeDo = () => {
   return (
     <div className="flex flex-col md:flex-row gap-2 bg-cst-neutral-1 p-2">
-      <div className="flex-1 bg-white rounded-2xl px-6 py-8 flex flex-col gap-4">
+      <div className="flex-1 bg-white rounded-2xl px-6 py-8 flex flex-col lg:gap-4">
         <SectionLabel text="What Do We Do" />
-        <h6 className="text-2xl font-semibold">
+        <AnimatedHeader>
+        <h6 className="text-2xl font-semibold mt-[20vh] lg:mt-0">
           We convert <span className="text-primary">Waste</span> and{" "}
           <span className="text-primary">Renewable Energy</span> into{" "}
           <span className="text-primary">Clean Hydrogen</span>. Our technology
@@ -16,43 +19,47 @@ const WhatDoWeDo = () => {
           <span className="text-primary">Cuts CO₂</span> emissions across
           industries.
         </h6>
+        </AnimatedHeader>
       </div>
-      {/* <div
-        className="flex-1 bg-white rounded-2xl px-6 py-8"
-        style={{
-          backgroundImage: `url(${globeImg.src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          transform: "rotate(-90deg)",
-          position: "fixed",
-        top: "-483.08px",
-        left: "-442px",
-        }}
-      >
-        Item 2
-      </div> */}
 
-      <div className="relative flex-1 bg-white rounded-2xl px-6 py-8 overflow-hidden">
+      <div className="relative flex-1 bg-white rounded-2xl overflow-hidden min-h-[50vh] lg:min-h-0">
         {/* Globe Image */}
-        <div className="absolute">
-          <Image
-            src={globeImg}
-            alt="Globe"
-            className="object-contain select-none"
-            draggable={false}
-            style={{
-              width: "1297.54px",
-              height: "973.15px",
-              transform: "rotate(-180deg)",
-              opacity: 1,
-              position: "absolute",
-              top: "-483.08px",
-              left: "-442px",
-            }}
-          />
+        <Image
+          src={globeImg}
+          alt="Globe"
+          className="object-cover select-none absolute"
+          fill
+          style={{
+            transform: "rotateY(-180deg)",
+            opacity: 1,
+            scale: "2",
+            top: "-100px",
+            left: "-100px",
+          }}
+        />
 
-          {/* Tablet version (hidden on desktop) */}
-          <Image
+        <FloatingTip
+          className="top-8 right-4 md:right-1/8 -translate-y-10 md:translate-0"
+          delay={1}
+        >
+          • 100% Carbon Value
+        </FloatingTip>
+
+        <FloatingTip
+          className="top-1/2 left-2/3 md:left-2/3 -translate-y-10 md:translate-0"
+          delay={0.2}
+        >
+          • 0% CO₂ Costs
+        </FloatingTip>
+        <FloatingTip
+          className="bottom-5 sm:bottom-5 left-1/9 -translate-y-10 md:translate-0"
+          delay={0.6}
+        >
+          • 50% Energy Savings
+        </FloatingTip>
+
+        {/* Tablet version (hidden on desktop) */}
+        {/* <Image
             src={globeImg}
             alt="Globe tablet"
             className="object-contain select-none md:hidden block"
@@ -66,26 +73,24 @@ const WhatDoWeDo = () => {
               top: "-238.78px",
               left: "-324.54px",
             }}
-          />
-        </div>
+          /> */}
+        {/* </div> */}
 
         {/* Text Labels */}
-        <div className="absolute inset-0 flex justify-center items-center">
-          {/* 100% Carbon Value */}
+        {/* <div className="absolute inset-0 flex justify-center items-center">
+
           <div className="absolute top-[28%] md:top-[20%] md:left-[55%] bg-white/80 backdrop-blur-md shadow-md rounded-lg px-4 py-2 text-sm font-semibold text-gray-900">
             <span className="text-blue-600">•</span> 100% Carbon Value
           </div>
 
-          {/* 50% Energy Savings */}
           <div className="absolute bottom-[18%] md:bottom-[25%] md:left-[28%] bg-white/80 backdrop-blur-md shadow-md rounded-lg px-4 py-2 text-sm font-semibold text-gray-900">
             <span className="text-blue-600">•</span> 50% Energy Savings
           </div>
 
-          {/* 0% CO₂ Costs */}
           <div className="absolute bottom-[22%] md:bottom-[30%] md:right-[25%] bg-white/80 backdrop-blur-md shadow-md rounded-lg px-4 py-2 text-sm font-semibold text-gray-900">
             <span className="text-blue-600">•</span> 0% CO₂ Costs
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/components/utils/heroutils/FloatingTip.jsx
+++ src/components/utils/heroutils/FloatingTip.jsx
@@ -4,7 +4,7 @@ import * as motion from "motion/react-client";
 export default function FloatingTip({ children, delay = 0, className = "" }) {
   return (
     <motion.div
-      className={`hidden md:block absolute bg-white/20 backdrop-blur-sm rounded-lg px-4 py-2 text-black text-xs md:text-sm font-bold shadow-lg z-2 ${className}`}
+      className={`absolute bg-white/20 backdrop-blur-sm rounded-lg px-4 py-2 text-black text-xs md:text-sm font-bold shadow-lg z-2 ${className}`}
       animate={{
         y: [-10, 10, -10],
       }}
