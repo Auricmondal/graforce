3638855c603d6d449b43d927c47ccb69d7cfc8e9
Sunanda-05 <mannasunanda11705@gmail.com>
2025-10-07 19:51:21 +0530

    Text size fix

--- src/components/sections/home/hero/HeroSection.jsx
+++ src/components/sections/home/hero/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
       <div className="absolute inset-0 flex flex-col items-center lg:items-start justify-between px-4 text-center lg:text-start text-white">
         <div className="flex-3 flex items-center">
           <AnimatedHeader delay={0.8}>
-            <h1 className="text-[clamp(56px,10vw,128px)] font-semibold max-w-8xl w-full px-6 leading-[110%]">
+            <h1 className="text-[clamp(40px,10vw,128px)] font-semibold max-w-8xl w-full px-6 leading-[110%]">
               Powering the Future with{" "}
               <span className="text-primary">Hydrogen</span>
             </h1>
@@ -53,13 +53,13 @@ const HeroSection = () => {
         <div className="flex-1 lg:flex-0 flex flex-col lg:flex-row items-center lg:justify-between w-full lg:pb-8 gap-4">
           <div className="flex gap-4">
             <PrimaryButton
-              className="bg-black/30 text-white transition border-1 border-white backdrop-blur-[17.4px] py-4 px-6 lg:py-8 lg:px-12 rounded-2xl lg:rounded-3xl font-medium lg:text-2xl flex items-center gap-3"
+              className="bg-black/30 text-white transition border-1 border-white backdrop-blur-[17.4px] py-3 px-4 md:py-4 md:px-6 lg:py-8 lg:px-12 rounded-2xl lg:rounded-3xl font-medium lg:text-2xl text-sm sm:text-base flex items-center gap-3"
               onClick={handleContactModal}
             >
               Book a Demo <FaChevronRight />
             </PrimaryButton>
             <PrimaryButton
-              className="bg-black/30 backdrop-blur-[9.7px] text-white py-4 px-6 lg:py-8 lg:px-12 rounded-2xl lg:rounded-3xl transition font-medium lg:text-2xl"
+              className="bg-black/30 backdrop-blur-[9.7px] text-white py-3 px-4 md:py-4 md:px-6 lg:py-8 lg:px-12 rounded-2xl lg:rounded-3xl transition font-medium lg:text-2xl text-sm sm:text-base"
               onClick={handleLearnMore}
             >
               Learn More
