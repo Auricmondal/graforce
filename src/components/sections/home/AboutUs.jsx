41507294c64e411c2b0c42991d1e60412179e93d
Anix <baranish2003@gmail.com>
2025-10-08 23:21:22 +0530

    Anix003/feat/home/aboutus 08.10.2025 11:22PM added home about us v2.1

--- src/components/sections/home/AboutUs.jsx
+++ src/components/sections/home/AboutUs.jsx
@@ -1,9 +1,16 @@
 "use client";
 
+import AnimatedHeader from "@/components/utils/animations/AnimatedHeader";
 import SectionLabel from "@/components/utils/badges/SectionLabel";
 import CardWrapper from "@/wrappers/CardWrapper";
 import SectionWrapper from "@/wrappers/SectionWrapper";
 import React, { useEffect, useState } from "react";
+import { LuClock3 } from "react-icons/lu";
+import { MdOutlineLocationOn } from "react-icons/md";
+import aboutUsImage from "@/assets/home/aboutimg.png";
+import Image from "next/image";
+import PrimaryButton from "@/components/utils/buttons/PrimaryButton";
+
 
 const AboutUs = () => {
   const brandLogoBase = "/brand/img/";
@@ -19,6 +26,8 @@ const AboutUs = () => {
   ];
 
   const [brandLogos, setBrandLogos] = useState(fallbackLogos); // default = fallback
+  const [foundation, setFoundation] = useState(2012);
+  const [location, setLocation] = useState("Berlin Adlershof");
   const [loading, setLoading] = useState(true);
 
   const API_URL = "/api/brands"; // replace with your real endpoint
@@ -67,9 +76,11 @@ const AboutUs = () => {
           align="center"
         >
           <SectionLabel text="About Us" />
-          <h2 className="text-3xl md:text-[48px] tracking-tight">
-            Trusted. Proven. Driven.
-          </h2>
+          <AnimatedHeader>
+            <h2 className="text-3xl md:text-[48px] tracking-tight">
+              Trusted. Proven. Driven.
+            </h2>
+          </AnimatedHeader>
         </CardWrapper>
 
         {loading ? (
@@ -77,12 +88,12 @@ const AboutUs = () => {
         ) : (
           <div
             id="brands"
-            className="grid md:grid-cols-3 grid-cols-2 gap-4 p-4 h-fit w-full bg-white rounded-lg"
+            className="grid md:grid-cols-3 grid-cols-2 gap-4 p-0 h-fit w-full bg-cst-neutral-1 rounded-lg"
           >
             {brandLogos.map((logo, i) => (
               <div
                 key={i}
-                className="group flex items-center justify-center bg-black/10 rounded-lg h-[131px] md:h-[196px]"
+                className="group flex items-center justify-center bg-cst-neutral-1 border border-cst-neutral-2 rounded-lg h-[131px] md:h-[196px]"
               >
                 <img
                   src={logo.src}
@@ -94,6 +105,66 @@ const AboutUs = () => {
             ))}
           </div>
         )}
+        <CardWrapper
+          variant="custom"
+          align="center"
+          color="default"
+          className="px-4 md:px-14 pt-[80px] pb-[112px]"
+        >
+          <div className="flex-col w-full">
+            <div className="flex flex-col sm:flex-row items-center justify-between gap-4 text-lg">
+              <div className="flex items-center gap-1">
+                <LuClock3 className="text-2xl" />Founded in {foundation}
+              </div>
+              <div className="flex items-center gap-1">
+                <MdOutlineLocationOn className="text-2xl" />{location}
+              </div>
+            </div>
+            <div className="flex-row pt-6 sm:pt-10 w-full">
+              {/* Partners + Image Section */}
+              <section className="grid grid-cols-1 md:grid-cols-2 gap-10 items-start w-full h-full">
+                {/* Partners */}
+                <div className="h-full">
+                  <div className="w-full h-full flex flex-col justify-between space-y-[80px] sm:space-y-[80px]">
+                    <div className="text-[48px] text-left capitalize font-bold">
+                      <h2 className="">
+                        <AnimatedHeader>
+                          graforce
+                        </AnimatedHeader>
+                      </h2>
+                    </div>
+                    <div className="">
+                      <AnimatedHeader>
+                        <p className="text-black text-base text-left">
+                          Power-to-X plants for the generation of CO2-free or CO2-negative hydrogen and synthetic raw materials the technology leader in sustainable solutions and negative emission technologies holds three patents for the application of different plasma processes {"("}high-frequency discharge, dielectric barrier discharge, coronal low-frequency discharge{")"} production capacities: Methane Plasmalysis  expansion to up to 50 MW/a through customers and partners
+                        </p>
+                      </AnimatedHeader>
+                      <div className="pt-6">
+                        <PrimaryButton className="bg-cst-neutral-5 text-white rounded-xl py-4 px-6 w-full">
+                          Learn More
+                        </PrimaryButton>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Technology Image */}
+                <div className="relative w-full h-full order-first md:order-last">
+                  <div className="absolute w-[100%] h-[100%] bg-cst-neutral-2 rounded-3xl z-0 translate-3"></div>
+                  <div className="relative z-10 rounded-3xl overflow-hidden w-[100%] h-[100%]">
+                    <Image
+                      src={aboutUsImage}
+                      alt="Graforce Technology"
+                      width={600}
+                      height={450}
+                      className="object-cover w-full h-full aspect-[3/4] sm:aspect-auto"
+                    />
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </CardWrapper>
       </div>
     </SectionWrapper>
   );
