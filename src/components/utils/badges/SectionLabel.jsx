e609059b4e0e08f2ca614caee7222914831f3275
Anix <baranish2003@gmail.com>
2025-10-09 04:25:28 +0530

    Anix003/feat/home/you-need-us 09.10.2025 04:09AM added home need us v1.2

--- src/components/utils/badges/SectionLabel.jsx
+++ src/components/utils/badges/SectionLabel.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import dropSvg from '@/assets/drop.svg';
 import Image from 'next/image';
 
-const SectionLabel = ({ text, textColor = "text-black" }) => {
+const SectionLabel = ({ icon = true, text, textColor = "text-black" }) => {
   return (
     <div className={`flex items-center space-x-2 ${textColor}`}>
-      <Image src={dropSvg} alt="Drop Icon" className="w-6 h-6" />
+      {icon && <Image src={dropSvg} alt="Drop Icon" className="w-6 h-6" />}
       <span className='capitalize'>{text}</span>
     </div>
   );
