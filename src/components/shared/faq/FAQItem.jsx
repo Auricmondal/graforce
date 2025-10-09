a396f0b8dbe75aeabd720fc5d1da9382217239ad
Sunanda-05 <mannasunanda11705@gmail.com>
2025-10-10 01:25:00 +0530

    FAQ section update

--- src/components/shared/faq/FAQItem.jsx
+++ src/components/shared/faq/FAQItem.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { FaPlus } from "react-icons/fa";
+import CardWrapper from "@/wrappers/CardWrapper";
 
 const questionVariants = {
   hidden: { opacity: 0, x: 50 },
@@ -19,15 +20,17 @@ export default function FAQItem({ question, answer }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="border-b border-[#0000004D] overflow-hidden flex flex-col my-[64px]">
+    <CardWrapper className="overflow-hidden flex flex-col p-6" variant="custom">
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className="w-full flex justify-between items-center mb-3 text-left cursor-pointer"
+        className={`w-full flex justify-between items-center pb-3 text-left transform ease-in-out cursor-pointer ${
+          isOpen ? "" : "border-b border-black/30"
+        }`}
       >
         {/* Animate only the question */}
         <motion.h4
           variants={questionVariants}
-          className="text-xl md:text-2xl lg:text-4xl font-semibold"
+          className="text-2xl font-semibold"
         >
           {question}
         </motion.h4>
@@ -44,12 +47,12 @@ export default function FAQItem({ question, answer }) {
         layout
         initial={false}
         animate={{ opacity: isOpen ? 1 : 0 }}
-        className={`text-gray-700 overflow-hidden transition-opacity duration-300 ${
-          isOpen ? "pb-8" : "pb-0 h-0"
+        className={`text-[#181818] overflow-hidden transition-opacity duration-300 ${
+          isOpen ? "pb-6" : "pb-0 h-0"
         }`}
       >
         {isOpen && <div>{answer}</div>}
       </motion.div>
-    </div>
+    </CardWrapper>
   );
 }
