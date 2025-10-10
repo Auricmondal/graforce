36efb47c44a17af018f5d479b49e4c1fab8ae89b
Sunanda-05 <mannasunanda11705@gmail.com>
2025-10-10 12:21:58 +0530

    FAQ animation update

--- src/components/shared/faq/FAQ.jsx
+++ src/components/shared/faq/FAQ.jsx
@@ -1,6 +1,9 @@
 "use client";
 
 import Image from "next/image";
+import React, { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 import { motion } from "motion/react";
 import FAQItem from "./FAQItem";
@@ -40,6 +43,28 @@ const faqs = [
 ];
 
 export default function FAQSection() {
+  const imageRef = useRef(null);
+  const faqContainerRef = useRef(null);
+
+  // GSAP animation function using hook
+  const { context: gsapCtx } = useGSAP(() => {
+    if (!imageRef.current || !faqContainerRef.current) return;
+
+    const resizeObserver = new ResizeObserver(() => {
+      const newHeight = faqContainerRef.current?.offsetHeight ?? 0;
+
+      gsap.to(imageRef.current, {
+        height: newHeight,
+        duration: 0.4,
+        ease: "power2.out",
+      });
+    });
+
+    resizeObserver.observe(faqContainerRef.current);
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
   return (
     <SectionWrapper
       className=""
@@ -47,47 +72,61 @@ export default function FAQSection() {
         background: "var(--cst-neutral-1)",
       }}
     >
-      <CardWrapper
-        className="rounded-lg gap-2 bg-white py-8 px-4 md:px-6"
-        variant="custom"
-      >
-        <div className="md:max-w-[400px] flex flex-col gap-2">
-          <SectionLabel text={"FAQs"} />
-          <AnimatedHeader>
-            <h2 className="text-xl md:text-[32px]">
-              Let&apos;s Answer Your Queries
-            </h2>
-          </AnimatedHeader>
-        </div>
-      </CardWrapper>
+      <motion.div layout>
+        <CardWrapper
+          className="rounded-lg gap-2 bg-white py-8 px-4 md:px-6"
+          variant="custom"
+        >
+          <div className="md:max-w-[400px] flex flex-col gap-2">
+            <SectionLabel text={"FAQs"} />
+            <AnimatedHeader>
+              <h2 className="text-xl md:text-[32px]">
+                Let&apos;s Answer Your Queries
+              </h2>
+            </AnimatedHeader>
+          </div>
+        </CardWrapper>
 
-      <div className="flex flex-col md:flex-row w-full h-fit gap-2 py-2">
-        <div className="hidden md:block md:flex-[1] lg:flex-[2] bg-primary rounded-lg">
-          <Image
-            src={shapeImg.src}
-            alt="Blue Image"
-            width={800}
-            height={800}
-            className="w-full h-full object-cover"
-          />
-        </div>
-
-        <div className="flex-1 md:flex-[4] lg:flex-[3]">
-          <motion.div
-            variants={containerVariants}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ amount: 0.2 }}
-            className="flex flex-col gap-2"
+        <motion.div
+          layout
+          className="flex flex-col md:flex-row w-full h-fit gap-2 py-2"
+          transition={{ duration: 5, type: "tween" }}
+        >
+          <div
+            style={{ height: "auto" }}
+            ref={imageRef}
+            className="hidden md:block md:flex-[1] lg:flex-[2] bg-primary rounded-lg"
           >
-            {faqs.map((faq, idx) => (
-              <FAQItem key={idx} question={faq.question} answer={faq.answer} />
-            ))}
-          </motion.div>
-        </div>
-      </div>
+            <Image
+              src={shapeImg.src}
+              alt="Blue Image"
+              width={800}
+              height={800}
+              className="w-full h-full object-cover"
+            />
+          </div>
 
-      {/* </div> */}
+          <motion.div layout className="flex-1 md:flex-[4] lg:flex-[3]">
+            <motion.div
+              layout
+              variants={containerVariants}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ amount: 0.2 }}
+              className="flex flex-col gap-2"
+              ref={faqContainerRef}
+            >
+              {faqs.map((faq, idx) => (
+                <FAQItem
+                  key={idx}
+                  question={faq.question}
+                  answer={faq.answer}
+                />
+              ))}
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </SectionWrapper>
   );
 }
--- src/components/shared/faq/FAQItem.jsx
+++ src/components/shared/faq/FAQItem.jsx
@@ -20,40 +20,43 @@ export default function FAQItem({ question, answer }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <CardWrapper className="overflow-hidden flex flex-col p-8" variant="custom">
-      <button
-        onClick={() => setIsOpen((prev) => !prev)}
-        className={`w-full flex justify-between items-center pb-3 text-left transform ease-in-out cursor-pointer ${
-          isOpen ? "" : "border-b border-black/30"
-        }`}
+    <motion.div layout>
+      <CardWrapper
+        className="overflow-hidden flex flex-col p-8"
+        variant="custom"
       >
-        {/* Animate only the question */}
-        <motion.h4
-          variants={questionVariants}
-          className="text-2xl font-semibold"
+        <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className={`w-full flex justify-between items-center pb-3 text-left transform ease-in-out cursor-pointer`}
         >
-          {question}
-        </motion.h4>
+          {/* Animate only the question */}
+          <motion.h4
+            variants={questionVariants}
+            className="text-2xl font-semibold"
+          >
+            {question}
+          </motion.h4>
+
+          <motion.div
+            animate={{ rotate: isOpen ? 45 : 0, scale: isOpen ? 1.2 : 1 }}
+            transition={{ duration: 0.3 }}
+          >
+            <FaPlus />
+          </motion.div>
+        </button>
 
         <motion.div
-          animate={{ rotate: isOpen ? 45 : 0, scale: isOpen ? 1.2 : 1 }}
-          transition={{ duration: 0.3 }}
+          layout
+          initial={false}
+          animate={{ opacity: isOpen ? 1 : 0, y: isOpen ? 0 : -10 }}
+          exit={{ opacity: 0, y: -10 }}
+          className={`text-[#181818] overflow-hidden transition-opacity duration-300 ${
+            isOpen ? "pb-6" : "pb-0 h-0"
+          }`}
         >
-          <FaPlus />
+          {isOpen && <div>{answer}</div>}
         </motion.div>
-      </button>
-
-      <motion.div
-        layout
-        initial={false}
-        animate={{ opacity: isOpen ? 1 : 0, y: isOpen ? 0 : -10 }}
-        exit={{ opacity: 0, y: -10 }}
-        className={`text-[#181818] overflow-hidden transition-opacity duration-300 ${
-          isOpen ? "pb-6" : "pb-0 h-0"
-        }`}
-      >
-        {isOpen && <div>{answer}</div>}
-      </motion.div>
-    </CardWrapper>
+      </CardWrapper>
+    </motion.div>
   );
 }
