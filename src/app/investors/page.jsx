056040f2f51701975ea768cad91b80c7e2b8eb79
Sunanda-05 <mannasunanda11705@gmail.com>
2025-10-16 22:49:15 +0530

    Impact section add

--- src/app/investors/page.jsx
+++ src/app/investors/page.jsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/sections/investors/Hero'
+import GraforceSolution from '@/components/sections/investors/ImpactOpportunity'
 import React from 'react'
 
 const Investors = () => {
   return (
     <div>
         <Hero/>
+        <GraforceSolution />
     </div>
   )
 }
