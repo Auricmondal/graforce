4e264c7689ffccb2ef94c85b2526d00861bbe762
Sunanda-05 <mannasunanda11705@gmail.com>
2025-10-20 20:16:41 +0530

    Job scroller update

--- src/components/sections/jobs/Hero.jsx
+++ src/components/sections/jobs/Hero.jsx
@@ -1,14 +1,8 @@
-// components/HeroSection.jsx
 import SectionWrapper from "@/wrappers/SectionWrapper";
-import Image from "next/image";
 import PrimaryButton from "@/components/utils/buttons/PrimaryButton";
 import { FaChevronRight } from "react-icons/fa";
-import jobImg1 from "@/assets/job/jobs-img1.jpg";
-import jobImg2 from "@/assets/job/jobs-img2.jpg";
-import jobImg3 from "@/assets/job/jobs-img3.jpg";
-import jobImg4 from "@/assets/job/jobs-img4.jpg";
-import jobImg5 from "@/assets/job/jobs-img5.jpg";
 import AnimatedHeader from "@/components/utils/animations/AnimatedHeader";
+import JobScroller from "./JobScroller";
 
 const images = [
   "/images/building.jpg",
@@ -54,49 +48,7 @@ export default function HeroSection() {
         </PrimaryButton>
       </div>
 
-      <div className="mt-12 grid grid-cols-2 grid-rows-27 gap-4 h-[654px]">
-        <div className="relative col-span-1 row-span-15">
-          <Image
-            src={jobImg1}
-            alt="Building with lit windows"
-            fill
-            className="object-cover rounded-lg "
-            priority
-          />
-        </div>
-        <div className="relative md:block rounded-lg overflow-hidden col-span-1 row-span-5">
-          <Image
-            src={jobImg3}
-            alt="Black and white columns"
-            fill
-            className="object-cover rounded-lg"
-          />
-        </div>
-        <div className="relative rounded-lg overflow-hidden col-span-1 row-span-15">
-          <Image
-            src={jobImg4}
-            alt="Woman with festive decoration"
-            fill
-            className="object-cover rounded-lg"
-          />
-        </div>
-        <div className="relative rounded-lg overflow-hidden col-span-1 row-span-14">
-          <Image
-            src={jobImg2}
-            alt="People walking on a bridge"
-            fill
-            className="object-cover rounded-lg"
-          />
-        </div>
-        <div className="relative rounded-lg overflow-hidden col-span-1 row-span-9">
-          <Image
-            src={jobImg5}
-            alt="Car headlight close up"
-            fill
-            className="object-cover rounded-lg"
-          />
-        </div>
-      </div>
+      <JobScroller />
     </SectionWrapper>
   );
 }
