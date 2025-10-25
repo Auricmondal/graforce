e320edfad4b177e938a5461761cc826e65691d1e
Auric  Mondal <auricmondal@gmail.com>
2025-10-25 20:14:05 +0530

    fix error

--- src/components/sections/about/OurTeam.jsx
+++ src/components/sections/about/OurTeam.jsx
@@ -1,24 +1,35 @@
-import AnimatedHeader from '@/components/utils/animations/AnimatedHeader'
-import SectionLabel from '@/components/utils/badges/SectionLabel'
-import CardWrapper from '@/wrappers/CardWrapper'
-import SectionWrapper from '@/wrappers/SectionWrapper'
-import React from 'react'
-import BrandLogos from '@/components/shared/brands/brands'
+import AnimatedHeader from "@/components/utils/animations/AnimatedHeader";
+import SectionLabel from "@/components/utils/badges/SectionLabel";
+import CardWrapper from "@/wrappers/CardWrapper";
+import SectionWrapper from "@/wrappers/SectionWrapper";
+import React from "react";
+import BrandLogos from "@/components/shared/brands/Brands";
 
 const OurTeam = () => {
   return (
     <div>
-      <SectionWrapper sectionClassName='bg-cst-neutral-1'>
-        <CardWrapper variant='custom' color='transparent' align='center' className='py-20 px-8 gap-4'>
+      <SectionWrapper sectionClassName="bg-cst-neutral-1">
+        <CardWrapper
+          variant="custom"
+          color="transparent"
+          align="center"
+          className="py-20 px-8 gap-4"
+        >
           <div className="flex flex-col justify-center items-center gap-4">
-            <SectionLabel text={'Our Team'} invertIcon={false} textColor='text-black' />
+            <SectionLabel
+              text={"Our Team"}
+              invertIcon={false}
+              textColor="text-black"
+            />
             <AnimatedHeader>
               <h2 className={`text-2xl md:text-3xl capitalize`}>
                 Backed By Top Brands
               </h2>
             </AnimatedHeader>
             <a>
-              <p className="text-center text-cst-neutral-5 underline hover:text-primary transition-colors duration-300 ease-in-out cursor-pointer">Explore More with Our News</p>
+              <p className="text-center text-cst-neutral-5 underline hover:text-primary transition-colors duration-300 ease-in-out cursor-pointer">
+                Explore More with Our News
+              </p>
             </a>
           </div>
           <div className="flex w-full justify-center items-center">
@@ -27,7 +38,7 @@ const OurTeam = () => {
         </CardWrapper>
       </SectionWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default OurTeam
+export default OurTeam;
