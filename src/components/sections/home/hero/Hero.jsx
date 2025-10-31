7a179f589a2622a7df0bd3fe282aee8e63b325e0
Sunanda-05 <mannasunanda11705@gmail.com>
2025-10-31 18:08:34 +0530

    home hero dynamic load

--- src/components/sections/home/hero/Hero.jsx
+++ src/components/sections/home/hero/Hero.jsx
@@ -1,7 +1,15 @@
 "use client";
 import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 
-import HeroSection from "@/components/sections/home/hero/HeroSection";
+// import HeroSection from "@/components/sections/home/hero/HeroSection";
+const HeroSection = dynamic(
+  () => import("@/components/sections/home/hero/HeroSection"),
+  {
+    ssr: false,
+    loading: () => <div className="bg-gray-200 h-64 w-full animate-pulse" />,
+  }
+);
 
 const Hero = () => {
   const [isFixed, setIsFixed] = useState(true);
